dc6ee162f0d0487b323e4b1e1a742a81d1760ceb
misti <yallalraj1307@gmail.com>
2020-07-08 15:13:56 +0530

    [Rajeshwari] ADDED: player postion at 0 and declared snake and ladder postions.

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -1,2 +1,11 @@
 #!/bin/bash -x
 echo "Welcome to Snake and Ladder Simulation ...!"
+
+ladder+=( "4" "9" "20" "28" "40" "52" "63" "74" )
+snake+=( "3" "17" "35" "45" "54" "62" "87" "93" "95" "99" )
+
+start=0
+
+echo "Game Starts"
+echo "player 1 is at $start now"
+
